32e3d8ace454ec1466b670137e1ee7230133b857
Narottam Slawski <slawskin@tcd.ie>
2026-02-05 16:38:32 +0000

    Added comments RM.py and removed some unnecessary code

--- backend/RM.py
+++ backend/RM.py
@@ -1,36 +1,45 @@
 import csv
 
+# Class for the racks
 class rack:
     def __init__(self, code):
+        # Set code to the inputted code
         self.code = code
 
+        # Set the default values for the variables below to none
         self.type = None
         self.generation = None
         self.powerNeed = None
         self.capacity = None
 
+        # Opens "racks.csv" as a readable file
         with open("racks.csv", 'r', newline = "") as rackfile:
-            reader = csv.DictReader(rackfile)
-            for row in reader:
+
+            # rackReader interprets the file as a dictionary with the headers as the keys
+            rackReader = csv.DictReader(rackfile)
+
+            # Reads the rows until the correct code is found
+            for row in rackReader:
                 if row["Code"] == self.code:
+                    # Set the variable values to the correct ones
                     self.type = row["Type"]
-                    self.generation = row["Generation"]
-                    self.generation = int(self.generation)
-                    self.powerNeed = row["Power Need (kW)"]
-                    self.powerNeed = int(self.powerNeed)
-                    self.capacity = row["Capacity Output (RSU)"]
-                    self.capacity = float(self.capacity)
+                    self.generation = int(row["Generation"])
+                    self.powerNeed = int(row["Power Need (kW)"])
+                    self.capacity = float(row["Capacity Output (RSU)"])
         
+        # If any of the variables apart from "code" are none, it raises an error
         if self.type is None or self.generation is None or self.powerNeed is None or self.capacity is None:
             raise ValueError("Not valid code")
 
-    
+    # Function to print all the values
     def print(self):
-        print("Code:", self.code, "  Generation:", self.generation, "  Power Consumption:", self.powerNeed, "kW  Capacity Output:", self.capacity)
+        print("Code:", self.code, "  Generation:", self.generation, "  Power Consumption:", self.powerNeed, "kW  Capacity Output:", self.capacity, "RSU")
 
-def main():
+"""
+def test():
     myRack = rack("a25")
     myRack.print()
     print(myRack.generation + 6)
 
-main()
+test()
+"""
