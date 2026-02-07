8b322429b28fdfe7a377762805063086a5ed72ad
Narottam Slawski <slawskin@tcd.ie>
2026-02-07 18:00:43 +0000

    Added color attribute to rack class as well as the json, as was in the file given by john

--- backend/RM.py
+++ backend/RM.py
@@ -20,6 +20,7 @@ class rack:
         self.generation = None
         self.powerNeed = None
         self.capacity = None
+        self.color = None
 
         # Get the correct rack row
         rack = racks.get(code)
@@ -31,20 +32,20 @@ class rack:
         self.generation = int(rack["Generation"])
         self.powerNeed = int(rack["Power Need (kW)"])
         self.capacity = float(rack["Capacity Output (RSU)"])
+        self.color = rack["Color"]
         
         # If any of the variables apart from "code" are none, it raises an error
-        if self.type is None or self.generation is None or self.powerNeed is None or self.capacity is None:
+        if self.type is None or self.generation is None or self.powerNeed is None or self.capacity is None or self.color is None:
             raise ValueError("Not valid code")
 
     # Function to print all the values
     def print(self):
-        print("Code:", self.code, "  Generation:", self.generation, "  Power Consumption:", self.powerNeed, "kW  Capacity Output:", self.capacity, "RSU")
+        print("Code:", self.code, "  Generation:", self.generation, "  Power Consumption:", self.powerNeed, "kW  Capacity Output:", self.capacity, "RSU  Color:", self.color)
 
-"""
+
 def test():
     myRack = rack("a25")
     myRack.print()
     print(myRack.generation + 6)
 
 test()
-"""
