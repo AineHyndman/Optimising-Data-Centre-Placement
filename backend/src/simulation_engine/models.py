f5902c49090549a623f3e3ce22b8e7e981180b92
AineHyndman <hyndmana@tcd.ie>
2026-02-19 02:21:58 +0000

    added more models to match up with frontend types

--- backend/src/simulation_engine/models.py
+++ backend/src/simulation_engine/models.py
@@ -1,11 +1,11 @@
 from dataclasses import dataclass, replace
-from typing import Dict, Tuple
+from typing import Dict, List, Optional
 from collections import Counter
 from src.model.position import Position
+from pydantic import BaseModel
 
 RackId = str
 Day = int
-
 
 @dataclass(frozen=True)
 class Rack:
@@ -14,7 +14,7 @@ class Rack:
     rack_type: str
     service: str
     year: int
-
+    color: str
 
 @dataclass(frozen=True)
 class SuiteState:
@@ -44,4 +44,48 @@ class SuiteState:
     def get_rsu_per_service(self):
         return Counter(r.service for r in self.racks.values())
 
+class Range(BaseModel):
+    min: float
+    max: float
 
+class Resources(BaseModel):
+    compute: float
+    storage: float
+    ai: float
+
+class Position(BaseModel):
+    rack_type: str
+    row: str
+    position: str
+
+class Constraints(BaseModel):
+    power_budget: int
+    compute_range: Range
+    storage_range: Range
+    ai_range: Range
+    generations: List[str]
+    generation_ratios: Optional[Dict[str,float]] = None
+
+class RackSpec(BaseModel):
+    name: str
+    type: str
+    color: str
+    generation: int
+    power_need: int
+    resources: Resources
+
+class SuitePlan(BaseModel):
+    datacenter: Optional[str] = None
+    suite: Optional[str] = None
+    total_power_usage: Optional[float] = None
+    compute: Optional[float] = None
+    storage: Optional[float] = None
+    ai: Optional[float] = None
+    generation_distribution: Optional[Dict[str,int]] = None
+    positions: List[Position]
+    day: Optional[int] = None
+
+class PlanData(BaseModel):
+    constraints: Constraints
+    rack_types: List[RackSpec]
+    cluster_plans: List[SuitePlan]
